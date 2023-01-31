582ecde7ca63dd3adea391696128c572cbbed651
Michael Lamar <mikewlamar@gmail.com>
2023-01-31 15:54:01 -0500

    mostly visual update

--- src/components/Fretboard.js
+++ src/components/Fretboard.js
@@ -1,13 +1,17 @@
 import { useState, useEffect } from 'react'
 import Course from './Course'
 
-const tuningData = [
-    { name: "Open C", id: "gceg", stringData: [392, 523.25, 659.25, 783.99] },
-    { name: "Viola", id: "cgda", stringData: [261.63, 392, 587.33, 880] },
-    { name: "Bass", id: "eadg", stringData: [329.63, 440, 587.33, 783.99] }
-]
+
 
 const Fretboard = ({ audioContext }) => {
+
+    const tuningData = [
+        { name: "Open C", id: "gceg", stringData: [392, 523.25, 659.25, 783.99] },
+        { name: "Viola", id: "cgda", stringData: [261.63, 392, 587.33, 880] },
+        { name: "Bass", id: "eadg", stringData: [329.63, 440, 587.33, 783.99] },
+        { name: "Guitar", id: "eadgbe", stringData: [329.63, 440, 587.33, 783.99, 987.77, 1318.51] }
+    ]
+
     const [showSettings, setShowSettings] = useState(false);
     const [gainValue, setGainValue] = useState(0.15)
 
@@ -15,22 +19,8 @@ const Fretboard = ({ audioContext }) => {
     const [courseFrequencies, setCourseFrequencies] = useState([392, 523.25, 659.25, 783.99])
 
     useEffect(() => {
-        console.log(selectedTuning)
-        if (selectedTuning === "Open C") {
-            console.log("Doing open c stuff")
-            let tuning = tuningData[0]
-            setCourseFrequencies(tuning.stringData)
-        } else if (selectedTuning === "Viola") {
-            console.log("doing Viola stuff")
-            let tuning = tuningData[1]
-            setCourseFrequencies(tuning.stringData)
-        } else if (selectedTuning === "Bass") {
-            console.log("doing Bass stuff")
-            let tuning = tuningData[2]
-            setCourseFrequencies(tuning.stringData)
-        } else {
-            console.log("no tuning selected")
-        }
+        let selectedTuningData = tuningData.find(tuning => tuning.name === selectedTuning)
+        setCourseFrequencies(selectedTuningData.stringData)
     }, [selectedTuning])
 
     function handleTuningChange(e) {
@@ -39,18 +29,24 @@ const Fretboard = ({ audioContext }) => {
 
     return (
         <>
-            <div className='controls'>
+            <header>
                 <button onClick={() => setShowSettings(!showSettings)}>Toggle Settings</button>
-                {showSettings && <div className='settings'>
-                    <div className='gain-control'>
-                        <input type="range" name="gain-control" id="gain-control" min={0} max={0.5} step={0.05} value={gainValue} onChange={e => setGainValue(e.target.value)} />
-                        <p>Gain: {gainValue}</p>
-                    </div>
-                    <div className='tuning-control'>
-                        Current tuning: {selectedTuning}
-                        {tuningData.map((tuning, index) => (
-                            <div key={index}>
-                                <label htmlFor={tuning.id}>{tuning.name}</label>
+            </header>
+
+            {showSettings && <div className='settings'>
+                <div className='settings-display'>
+                    <p>Current tuning: {selectedTuning}</p>
+                </div>
+                <div className='gain-control'>
+                    <p>Gain: {gainValue}</p>
+                    <input type="range" name="gain-control" id="gain-control" min={0} max={0.5} step={0.05} value={gainValue} onChange={e => setGainValue(e.target.value)} />
+                </div>
+                <div className='tuning-control'>
+                    {tuningData.map((tuning, index) => (
+                        <div className='tuning-container' key={index}>
+
+
+                            <label htmlFor={tuning.id}>
                                 <input
                                     type="radio"
                                     name="tuning"
@@ -58,11 +54,13 @@ const Fretboard = ({ audioContext }) => {
                                     value={tuning.name}
                                     checked={selectedTuning === tuning.name}
                                     onChange={handleTuningChange} />
-                            </div>
-                        ))}
-                    </div>
-                </div>}
-            </div>
+                                <span>{tuning.name}</span>
+                            </label>
+                        </div>
+                    ))}
+                </div>
+            </div>}
+
             <div className='fretboard'>
                 {courseFrequencies.map((freq, index) => (
                     <div className='course' key={index}>
--- src/components/Key.js
+++ src/components/Key.js
@@ -13,23 +13,6 @@ const Key = ({ audioContext, gainValue = 0.25, fretName, frequency }) => {
         setKeyGainValue(gainValue)
     }, [gainValue])
 
-    function hertzToNote(hertz) {
-        let notes = ["C", "C#Db", "D", "D#Eb", "E", "F", "F#Gb", "G", "G#Ab", "A", "A#Bb", "B"];
-        let index = 12 * (Math.log(hertz / 440) / Math.log(2)) + 57;
-        let octave = Math.floor(index / 12);
-        let note = notes[Math.round(index) % 12] + octave;
-        return note;
-    }
-
-    //not using this function right now but its handy to keep around
-    function noteToHertz(note) {
-        let notes = { "C": 16.35, "C#": 17.32, "D": 18.35, "D#": 19.45, "E": 20.60, "F": 21.83, "F#": 23.12, "G": 24.50, "G#": 25.96, "A": 27.50, "A#": 29.14, "B": 30.87 };
-        let noteName = note.slice(0, -1);
-        let octave = parseInt(note.slice(-1));
-        let hertz = notes[noteName] * Math.pow(2, octave - 4);
-        return hertz;
-    }
-
     function makeBeep(freq, wave) {
         let attackTime = 0.03
         let sustainTime = 0.15
@@ -54,6 +37,23 @@ const Key = ({ audioContext, gainValue = 0.25, fretName, frequency }) => {
     const handleKeyDown = () => {
         makeBeep(frequency, "triangle")
         makeBeep(frequency, "sine")
+    }
+
+    function hertzToNote(hertz) {
+        let notes = ["C", "C#Db", "D", "D#Eb", "E", "F", "F#Gb", "G", "G#Ab", "A", "A#Bb", "B"];
+        let index = 12 * (Math.log(hertz / 440) / Math.log(2)) + 57;
+        let octave = Math.floor(index / 12);
+        let note = notes[Math.round(index) % 12] + octave;
+        return note;
+    }
+
+    //not using this function right now but its handy to keep around
+    function noteToHertz(note) {
+        let notes = { "C": 16.35, "C#": 17.32, "D": 18.35, "D#": 19.45, "E": 20.60, "F": 21.83, "F#": 23.12, "G": 24.50, "G#": 25.96, "A": 27.50, "A#": 29.14, "B": 30.87 };
+        let noteName = note.slice(0, -1);
+        let octave = parseInt(note.slice(-1));
+        let hertz = notes[noteName] * Math.pow(2, octave - 4);
+        return hertz;
     }
 
     return (
